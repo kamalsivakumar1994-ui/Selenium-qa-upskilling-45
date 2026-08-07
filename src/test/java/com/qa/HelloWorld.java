bf404c46f07aed0d8cd6965019987140e7512de3
Kamal <kamals@Lifepowr.io>
2026-08-07 08:59:27 +0200

    chore: add gitignore, remove IDE files, clean setup

---
+++ src/test/java/com/qa/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.qa;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, SDET!");
+        System.out.println("Day 1 of 45. Let's go.");
+    }
+}
